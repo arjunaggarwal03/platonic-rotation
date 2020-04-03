f9c7d43fa3a15828709ce63ee5453bae6685d4a4
arjun aggarwal <arjun.2003.mail@gmail.com>
2020-04-03 07:46:20 -0400

    added rotation

--- polyhedrons/dodecahedron.cpp
+++ polyhedrons/dodecahedron.cpp
@@ -51,3 +51,44 @@ int main(){
 
   //Dodecahedron
   vector<vector<float>> points{{0,w/(gr*6),w*gr/6},{0,-w/(gr*6),-w*gr/6},{0,w/(gr*6),-w*gr/6},{0,-w/(gr*6),w*gr/6},{w*gr/6,0,w/(gr*6)},{-w*gr/6,0,-w/(gr*6)},{-w*gr/6,0,w/(gr*6)},{w*gr/6,0,-w/(gr*6)},{w/(gr*6),w*gr/6,0},{-w/(gr*6),-w*gr/6,0},{-w/(gr*6),w*gr/6,0},{w/(gr*6),-w*gr/6,0},{-w/6,-w/6,-w/6},{w/6,-w/6,-w/6},{w/6,w/6,-w/6},{-w/6,w/6,-w/6},{-w/6,-w/6,w/6},{w/6,-w/6,w/6},{w/6,w/6,w/6},{-w/6,w/6,w/6}};
+
+  int delay = 0;
+  
+  while (delay < 10000){
+    //rotation matrices
+    vector<vector<float>> rotationZ{{cos(angle),-sin(angle),0},{sin(angle),cos(angle),0},{0,0,1}};
+    vector<vector<float>> rotationX{{1,0,0},{0,cos(angle),-sin(angle)},{0,sin(angle),cos(angle)}};
+    vector<vector<float>> rotationY{{cos(angle),0,-sin(angle)},{0,1,0},{sin(angle),0,cos(angle)}};
+
+    vector<Point> pts;
+
+    for(int i=0; i < points.size(); i++){
+      vector<vector<float>> rotated = matmul(rotationY,points[i]);
+      rotated = matmul(rotationX,rotated);
+      rotated = matmul(rotationY,rotated);
+
+      float distance = 2;
+      float z = 1/(2 - rotated[2][0]);
+      cout << z << endl;
+      vector<vector<float>> projection{{z,0,0},{0,z,0}};
+
+      vector<vector<float>> projected2d = matmul(projection, rotated);
+      drawPoint(image,Point(projected2d[0][0]+w/2,projected2d[1][0]+w/2));
+      pts.push_back(Point(projected2d[0][0]+w/2,projected2d[1][0]+w/2));
+    }
+
+    imshow("Animation", image);
+    waitKey(1);
+    image = Mat::zeros(w, w, CV_8UC3); //resets image
+    angle += 0.02; //update angle
+    delay++;
+  }
+}
+
+void drawLine( Mat img, Point start, Point end ){
+  line( img,start,end,Scalar(255, 255, 0),2,8);
+}
+
+void drawPoint( Mat img, Point temp){
+  circle(img,temp,2,Scalar(255,158,179),18,8,0);
+}
